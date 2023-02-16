e9810fe17ec0475d49e8ccea5286a8c45940ffae
davidebschke <davidebschke8@gmail.com>
2023-02-16 11:19:39 +0100

    add webclient for jobdetails getAllJobs

--- backend/src/main/java/com/example/backend/jobs/JobService.java
+++ backend/src/main/java/com/example/backend/jobs/JobService.java
@@ -1,13 +1,25 @@
 package com.example.backend.jobs;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class JobService {
+
+    String apiUrl = "https://rest.arbeitsagentur.de/jobboerse/jobsuche-service/pc/v4/app/jobs";
+
     public List<Jobdetails> getAllJobs() {
 
-        return null;
+        WebClient client = WebClient.create();
+
+        return Objects.requireNonNull(Objects.requireNonNull(client.get()
+                        .uri(apiUrl).
+                        retrieve().
+                        toEntityList(Jobdetails.class)
+                        .block())
+                .getBody());
     }
 }
